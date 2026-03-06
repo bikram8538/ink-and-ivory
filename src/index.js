0409ea3b67236153970d7887db7e42627294f568
bikram8538 <bikram8538@gmail.com>
2026-03-06 20:02:35 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -13,13 +13,53 @@ var swiper = new Swiper(".bookSwiper", {
   },
   breakpoints: {
     320: {
-      slidesPerView: 1
+      slidesPerView: 1,
     },
     768: {
-      slidesPerView: 3
+      slidesPerView: 3,
     },
     1024: {
-      slidesPerView: 5
-    }
-  }
+      slidesPerView: 5,
+    },
+  },
+});
+
+var swiper = new Swiper(".sellerSwiper", {
+  // effect: "coverflow",
+  slidesPerView: 1.5,
+  spaceBetween: 10,
+  loop: true,
+  // coverflowEffect: {
+  //   rotate: 0,
+  //   stretch: 0,
+  //   depth: 400,
+  //   modifier: 1,
+  //   slideShadows: false,
+  // },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  /* autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },*/
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 1,
+    },
+    992: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 2,
+    },
+  },
 });
